11aa6e78e04ac75b96d824e551d10bf5c4a37e29
chijiokeprince92 <chijioke.nwaiwu92@gmail.com>
2022-05-05 17:35:52 +0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import multer from "multer";
+
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import postRoute from "./routes/posts.js";
+import conversationRoute from "./routes/conversations.js";
+import messageRoute from "./routes/messages.js";
+import path from "path";
+import {fileURLToPath} from 'url';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log('directory-name 👉️', __dirname);
+
+dotenv.config();
+
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
